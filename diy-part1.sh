d00541843db7f829f198a39e602d3156beb8d185
zzrove <58414473+zzrove@users.noreply.github.com>
2022-11-16 10:26:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+set -x
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+CONFIG_FILE="$1"
+
+if echo "$CONFIG_FILE"|grep -i GL-SFT1200
+then
+    :
+else
+    # https://github.com/coolsnowwolf/openwrt-gl-ax1800/blob/master/feeds.conf.default
+    # https://github.com/kenzok8/openwrt-packages
+    # https://github.com/kenzok8/small
+    echo '
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+    ls -lh package/utils/ucode
+    svn co https://github.com/coolsnowwolf/lede/trunk/package/utils/ucode package/utils/ucode
+    ls -lh package/utils/ucode
+fi
